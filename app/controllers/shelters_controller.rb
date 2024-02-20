c2e32e5b440123700423d649607e0afa8baf5d35
Karenina Wongsordjo <karenina2798@gmail.com>
2024-02-20 16:35:06 +0900

    new branch

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -1,5 +1,2 @@
 class SheltersController < ApplicationController
-    def show
-
-    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :shelters, dependent: :destroy
-  has_many :bookings, dependent: :destroy
-  has_many :bookings, through: :shelters, dependent: :destroy
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :name, presence: true
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
 end
