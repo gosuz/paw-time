1c68963c9825d446168f1c8988274392fc926ab0
Go Suzuki <go.suz55@gmail.com>
2024-02-20 14:22:40 +0900

    cloudinary set up

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,6 +1,8 @@
 class Shelter < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  # added this line
+  has_many_attached :photos
 
   validates :name, :location, :user, presence: true
   validates :capacity, numericality: { greater_than: 0 }, presence: true
