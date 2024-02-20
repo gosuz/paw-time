80c3847c6873fd751b3b2d0543327e3b40d5bc3e
Sarah Amour <samour621@gmail.com>
2024-02-20 18:43:44 +0900

    updated the banner

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -1,5 +1,5 @@
 class SheltersController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index]
   def index
     @shelters = Shelter.all
   end
