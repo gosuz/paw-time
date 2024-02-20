3e0078fd87a96bdecdadd020f11bd1d3c378b343
Hellen Tamara <hellen.tamara@outlook.com>
2024-02-20 14:46:24 +0900

    created booking create action, and provisional form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resource :shelters do
-    resource :bookings, only: [:index, :add, :edit, :destroy]
+  resources :shelters do
+    resources :bookings, only: [:index, :add, :edit, :destroy]
   end
   root to: "shelters#index"
 end
