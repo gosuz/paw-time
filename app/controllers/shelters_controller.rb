155966f105b22f27b843932fb4915efbdaa3c4bb
Go Suzuki <go.suz55@gmail.com>
2024-02-20 18:28:55 +0900

    Revert "added new and create to shelter controller and the form for shelters, and new view for shelters"
    This reverts commit 04d4a474136bbc30910eba6d385e26b5c0fc9be4.

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -1,26 +1,2 @@
 class SheltersController < ApplicationController
-  def index
-    @shelters = Shelter.all
-  end
-  # delete later
-
-  def new
-    @shelter = Shelter.new
-  end
-
-  def create
-    # Going to post the form to where????
-    @shelter = Shelter.new(shelter_params)
-    if @shelter.save
-      # redirect_to shelter they just created
-      redirect_to shelter_path(@shelter)
-    else
-
-    end
-  end
-
-  private
-  def shelter_params
-    @shelter.require(:shelter).permit(:name, :location, :capacity, :logo, :banner, :description)
-  end
 end
