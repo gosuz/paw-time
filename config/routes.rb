8cdb9bac4dd1df179249c789f980439858e8139a
Sarah <134185886+Samour621@users.noreply.github.com>
2024-02-20 14:55:31 +0900

    Wrong action. Add to Create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :shelters do
-    resources :bookings, only: [:index, :add, :edit, :destroy]
+    resources :bookings, only: [:index, :create, :edit, :destroy]
   end
   root to: "shelters#index"
 end
