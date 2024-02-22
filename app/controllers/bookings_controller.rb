b1c836cdaeeb9b7ad15d1e4ecccafa1e745ed779
Hellen Tamara <hellen.tamara@outlook.com>
2024-02-22 14:28:39 +0900

    update option added to controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -18,11 +18,16 @@ class BookingsController < ApplicationController
 
   def update
     @booking = Booking.find(params[:id])
+    if @booking.update(booking_params)
+      redirect_to bookings_path
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 
   private
 
   def bookings_params
-    params.require(:booking).permit(:date, :reason, :time_slot, :status)
+    params.require(:booking).permit(:date, :reason, :time_slot, :status, :start_time, :end_time)
   end
 end
