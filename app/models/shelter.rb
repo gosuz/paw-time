14151138dda0ca5bee7630b64301e97054487b38
Sarah Amour <samour621@gmail.com>
2024-02-22 12:50:14 +0900

    seeds

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -2,8 +2,8 @@ class Shelter < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :pets, dependent: :destroy
-  has_one_attached :logo
-  has_one_attached :banner
+  # has_one_attached :logo
+  # has_one_attached :banner
 
   validates :name, :location, :user, presence: true
   validates :capacity, numericality: { greater_than: 0 }, presence: true
