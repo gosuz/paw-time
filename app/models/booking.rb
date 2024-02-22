8daabff9c75d43e3d99d44ccf267df7836619a95
Hellen Tamara <hellen.tamara@outlook.com>
2024-02-22 12:41:19 +0900

    fixing seeds

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,6 @@ class Booking < ApplicationRecord
   belongs_to :shelter
   belongs_to :user
 
-  validates :date, :time_slot, :shelter, :user, presence: true
+  validates :date, :time_slot,  presence: true
   validates :reason, presence: true
 end
--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -2,9 +2,9 @@ class Shelter < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :pets, dependent: :destroy
-  has_one_attached :logo
-  has_one_attached :banner
+  has_one_attached :c_logo
+  has_one_attached :c_banner
 
-  validates :name, :location, :user, presence: true
+  validates :name, :location, presence: true
   validates :capacity, numericality: { greater_than: 0 }, presence: true
 end
