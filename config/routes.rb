7e66f22317c3aef84e2429a7177b0a4c9adf8cb7
Hellen Tamara <hellen.tamara@outlook.com>
2024-02-22 17:03:54 +0900

    updating status work

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: "shelters#index"
   resources :shelters do
-    resources :bookings, only: [:create, :edit, :update, :destroy, :new]
+    resources :bookings, only: [:create]
     resources :pets, only: [:new, :create]
   end
 
-  resources :bookings, only: [:index]
-  root to: "shelters#index"
+  resources :bookings, only: [:index, :update, :edit, :destroy, :create]
+
 end
