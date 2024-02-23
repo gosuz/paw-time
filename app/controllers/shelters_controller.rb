93a7c36936a8428b0fd33949485c65480b3b2f74
Hellen Tâmara <105838459+HellenTamara@users.noreply.github.com>
2024-02-23 12:50:33 +0900

    Revert "Pet controller creation action and made form work"

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -6,8 +6,8 @@ class SheltersController < ApplicationController
 
   def show
     @shelter = Shelter.find(params[:id])
-    @booking = Booking.new
     @pet = Pet.new
+    @booking = Booking.new
   end
 
   def new
